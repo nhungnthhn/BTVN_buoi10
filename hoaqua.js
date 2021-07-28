d43444b826aec5e058478122d0e78930fb3f8b2b
nhungnthhn <86668997+nhungnthhn@users.noreply.github.com>
2021-07-28 23:24:56 +0700

    hoa qua

---
+++ hoaqua.js
@@ -0,0 +1,27 @@
+function hoaqua() {
+    let cost = document.getElementById("hoaqua1").value;
+    switch (cost) {
+        case '1':
+        document.getElementById(result).innerHTML = ("Giá " + cost + "là: 20000VND/kg")
+        break;
+        case '2':
+        document.getElementById(result).innerHTML = ("Giá " + cost + "là: 20000VND/kg")
+        break;
+        case '3':
+        document.getElementById(result).innerHTML = ("Giá " + cost + "là: 30000VND/kg")
+        break;
+        case '4':
+        document.getElementById(result).innerHTML = ("Giá " + cost + "là: 30000VND/kg")
+        break;
+        case '5':
+        document.getElementById(result).innerHTML = ("Giá " + cost + "là: 40000VND/kg")
+        break;
+        case '6':
+        document.getElementById(result).innerHTML = ("Giá " + cost + "là: 40000VND/kg")
+        break;
+        case '7':
+        document.getElementById(result).innerHTML = ("Giá " + cost + "là: 50000VND/kg")
+        break;
+        default document.getElementById(result).innerHTML = ("Không có sản phẩm này")
+    }
+}
