d22809ef559b61703eaa1b58e0043f75dac2f4a1
nhungnthhn <86668997+nhungnthhn@users.noreply.github.com>
2021-07-29 00:00:22 +0700

    sửa msg thông báo kq ra màn hình

--- hoaqua.js
+++ hoaqua.js
@@ -2,26 +2,25 @@ function hoaqua() {
     let cost = document.getElementById("hoaqua1").value;
     switch (cost) {
         case '1':
-        document.getElementById("result").innerHTML = "Giá " + cost + "là: 20000VND/kg";
+        document.getElementById("result").innerHTML = "Giá ổi là: 20000VND/kg";
         break;
         case '2':
-        document.getElementById("result").innerHTML = "Giá " + cost + "là: 20000VND/kg";
+        document.getElementById("result").innerHTML = "Giá dưa hấu là: 20000VND/kg";
         break;
         case '3':
-        document.getElementById("result").innerHTML = "Giá " + cost + "là: 30000VND/kg";
+        document.getElementById("result").innerHTML = "Giá táo là: 30000VND/kg";
         break;
         case '4':
-        document.getElementById("result").innerHTML = "Giá " + cost + "là: 30000VND/kg";
+        document.getElementById("result").innerHTML = "Giá xoài là: 30000VND/kg";
         break;
         case '5':
-        document.getElementById("result").innerHTML = "Giá " + cost + "là: 40000VND/kg";
+        document.getElementById("result").innerHTML = "Giá cam là: 40000VND/kg";
         break;
         case '6':
-        document.getElementById("result").innerHTML = "Giá " + cost + "là: 40000VND/kg";
+        document.getElementById("result").innerHTML = "Giá chôm chôm là: 40000VND/kg";
         break;
         case '7':
-        document.getElementById("result").innerHTML = "Giá " + cost + "là: 50000VND/kg";
+        document.getElementById("result").innerHTML = "Giá măng cụt là: 50000VND/kg";
         break;
-        default document.getElementById("result").innerHTML = "Không có sản phẩm này";
     }
 }
