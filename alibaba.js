dec378b701e57d594f1840a2dde0c57291ad8336
Nhung <nhungnth@relipasoft.com>
2021-08-03 19:46:22 +0700

    alibaba

---
+++ alibaba.js
@@ -0,0 +1,12 @@
+function check() {
+    debugger
+    let password = "Vung oi mo ra";
+    let a;
+    do {
+        
+        a = document.getElementById("password").value;
+    }
+    while (a !== password){
+        alert("Chào mừng");
+    } 
+}
